a5a7546206f6a8d5633b6cb9a0b13e271cbd52d5
zidane0000 <zidane0917@gmial.com>
2018-04-13 14:55:00 +0800

    2018/4/13 實驗室電腦 pieview

--- app/src/main/java/tw/edu/niu/keepmoney20/menu_account.java
+++ app/src/main/java/tw/edu/niu/keepmoney20/menu_account.java
@@ -7,22 +7,16 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
 
 import java.util.ArrayList;
 
+import im.dacer.androidcharts.PieHelper;
+import im.dacer.androidcharts.PieView;
+
 public class menu_account extends AppCompatActivity {
 
-    private float[] price= {1,2,3};
-    private String[] xData={"食物","交通","其他"};
-    PieChart menu_account_pie;
+
     private ListView toaccount_listView;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> toaccount_listview_data = new ArrayList<>();
@@ -34,48 +28,15 @@ public class menu_account extends AppCompatActivity {
         //強制鎖定為直屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        menu_account_pie = (PieChart) findViewById(R.id.idPieChart);
-
-        menu_account_pie.setRotationEnabled(true);//不可旋轉
-
-        Description description = new Description();
-        description.setText("");
-        menu_account_pie.setDescription(description);//不顯示左下角的字
-
-        //pieChart.setUsePercentValues(true);
-        //pieChart.setHoleColor(Color.BLUE);
-        menu_account_pie.setHoleRadius(0f);//中心不留空洞
-        menu_account_pie.setTransparentCircleAlpha(0);//不需要有透明的第二個小圈
-        //menu_account_pie.setCenterText("Super Cool Chart");
-        //menu_account_pie.setCenterTextSize(5);
-        //pieChart.setDrawEntryLabels(true);
-        //pieChart.setEntryLabelTextSize(20);
-        //More options just check out the documentation!
-
-        addDataSet();
-
-        //案下去會有字浮出來
-        menu_account_pie.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-            @Override
-            public void onValueSelected(Entry e, Highlight h) {
-//                int pos1 = e.toString().indexOf("(sum): ");
-//                String sales = e.toString().substring(pos1 + 2);
-//
-//                for(int i = 0; i < price.length; i++){
-//                    if(price[i] == Float.parseFloat(sales)){
-//                        pos1 = i;
-//                        break;
-//                    }
-//                }
-//                String employee = xData[pos1 + 1];
-//                Toast.makeText(menu_account.this, "Employee "  + "\n" + "Sales: $"  + "K", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected() {
-
-            }
-        });
+        PieView pieView = (PieView)findViewById(R.id.pie_view);
+        ArrayList<PieHelper> pieHelperArrayList = new ArrayList<PieHelper>();
+        pieHelperArrayList.add(new PieHelper(20, Color.BLACK));
+        pieHelperArrayList.add(new PieHelper(6));
+        pieHelperArrayList.add(new PieHelper(30));
+        pieHelperArrayList.add(new PieHelper(12));
+        pieHelperArrayList.add(new PieHelper(32));
+        pieView.setDate(pieHelperArrayList);
+        pieView.showPercentLabel(false); //是否顯示百分比
 
         // 準備資料，塞3個項目到ArrayList裡
         for(int i = 0; i < 3; i++) {
@@ -94,38 +55,5 @@ public class menu_account extends AppCompatActivity {
 
     }
 
-    private void addDataSet() {
-        ArrayList<PieEntry> yEntrys = new ArrayList<>();
-        ArrayList<String> xEntrys = new ArrayList<>();
 
-        for(int i = 0; i < price.length; i++){
-            yEntrys.add(new PieEntry(price[i] , i));
-        }
-
-        for(int i = 1; i < xData.length; i++){
-            xEntrys.add(xData[i]);
-        }
-
-        //create the data set
-        PieDataSet pieDataSet = new PieDataSet(yEntrys, "");
-        pieDataSet.setSliceSpace(2);
-        pieDataSet.setValueTextSize(12);
-
-        //add colors to dataset
-        ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.BLUE);
-        colors.add(Color.GRAY);
-        colors.add(Color.GREEN);
-        colors.add(Color.CYAN);
-        colors.add(Color.YELLOW);
-        colors.add(Color.MAGENTA);
-
-        pieDataSet.setColors(colors);
-
-        //create pie data object
-        PieData pieData = new PieData(pieDataSet);
-        menu_account_pie.setData(pieData);
-        menu_account_pie.invalidate();
-    }
 }
